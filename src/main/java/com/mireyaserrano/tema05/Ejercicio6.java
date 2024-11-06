518dfb38b1873bef587766a2507cfdec0d2f67a4
= <=>
2024-11-06 17:12:13 +0100

    ej 6 estructuras de datos

--- src/main/java/com/mireyaserrano/tema05/Ejercicio6.java
+++ src/main/java/com/mireyaserrano/tema05/Ejercicio6.java
@@ -1,4 +1,29 @@
 package com.mireyaserrano.tema05;
 
+import java.util.Scanner;
+
 public class Ejercicio6 {
+    public static void main(String args[]){
+        String fraseRepetida = "";
+        int factorMultiplicacion;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce una frase");
+        String frase = scanner.nextLine();
+        do{
+            System.out.println("Introduce un factor de multiplicacion");
+            factorMultiplicacion = Integer.parseInt(scanner.nextLine());
+            if (factorMultiplicacion < 0){
+                System.err.println("ERROR, Introduce un numero valido");
+            }
+        }while (factorMultiplicacion < 0);
+        scanner.close();
+        for (int i = 0; i <= factorMultiplicacion; i++){
+            switch (i){
+                case 0 -> fraseRepetida = "";
+                case 1 -> fraseRepetida = frase;
+                default -> fraseRepetida = fraseRepetida.concat(frase);
+            }
+        }
+        System.out.println(fraseRepetida);
+    }
 }
