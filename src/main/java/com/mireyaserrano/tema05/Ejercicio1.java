de6b4a79298f4a3863b81b43fe582d19d12cf594
Mireya Serrano <mireyaserrano181105@gmail.com>
2024-11-06 10:46:02 +0100

    ej 1 estructuras de datos

---
+++ src/main/java/com/mireyaserrano/tema05/Ejercicio1.java
@@ -0,0 +1,25 @@
+package com.mireyaserrano.tema05;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    /**
+     * Cambia la primera letra de minuscula a mayuscula
+     * @param frase El string frase que se introduce
+     * @return La primera letra en mayuscula concatenada a el resto de la frase separado por un substring
+     */
+    public static String primeraMyuscula(String frase){
+        char primeraLetra = frase.charAt(0);
+        String minuscula = String.valueOf(primeraLetra);
+        String mayuscula = minuscula.toUpperCase();
+        String fraseSinPrimeraLetra = frase.substring(1);
+        return mayuscula.concat(fraseSinPrimeraLetra);
+    }
+
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce una frase:");
+        String frase = scanner.nextLine();
+        System.out.println(primeraMyuscula(frase));
+    }
+}
