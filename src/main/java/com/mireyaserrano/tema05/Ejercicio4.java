e2f193d43d2e0ac0f6558b233d83d8411fd730c0
= <=>
2024-11-06 16:53:30 +0100

    ej 4 estructuras de datos

--- src/main/java/com/mireyaserrano/tema05/Ejercicio4.java
+++ src/main/java/com/mireyaserrano/tema05/Ejercicio4.java
@@ -8,26 +8,22 @@ public class Ejercicio4 {
         System.out.println("Introduce una frase y te digo las mayusculas y minusculas que hay en cada palabra");
         String frase = scanner.nextLine();
         String palabra;
-        int mayusculas = 0;
-        int minusculas = 0;
+        int vocales = 0;
+        int consonantes = 0;
         scanner.close();
         String [] palabras = frase.split("\\s+");
         for (int i = 0; i < palabras.length; i++){
             palabra = palabras[i];
-            mayusculas = 0;
-            minusculas = 0;
+            vocales = 0;
+            consonantes = 0;
             for (int j = 0; j < palabra.length(); j++){
-                char letra = frase.charAt(j);
-                String letraStr = String.valueOf(letra);
-                if (letraStr.equals(" ")) {
-                    System.out.print("");
-                }else if (letraStr.equals(letraStr.toUpperCase())){
-                    mayusculas += 1;
-                }else {
-                    minusculas += 1;
+                char letra = palabras[i].charAt(j);
+                switch (letra){
+                    case 'a', 'e', 'i', 'o', 'u' -> vocales +=1;
+                    case 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' -> consonantes +=1;
                 }
             }
-            System.out.printf("La palabra %d, tiene %d mayusculas y %d minusculas\n", i+1, mayusculas, minusculas );
+            System.out.printf("La palabra %s tiene: %d vocales y %d consonantes\n", palabra, vocales, consonantes);
         }
     }
 }
