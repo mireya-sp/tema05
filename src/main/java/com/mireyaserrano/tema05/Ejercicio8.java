f0a8e67525e6ebebe76c415516873dfbc57b33bd
= <=>
2024-11-07 17:05:03 +0100

    ej 7 estructuras de datos

--- src/main/java/com/mireyaserrano/tema05/Ejercicio8.java
+++ src/main/java/com/mireyaserrano/tema05/Ejercicio8.java
@@ -1,4 +1,15 @@
 package com.mireyaserrano.tema05;
 
+import java.util.Scanner;
+
 public class Ejercicio8 {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce una frase");
+        String frase = scanner.nextLine();
+        String [] palabras = frase.split("\\s+");
+        for (int i = 0; i < palabras.length; i++){
+            System.out.printf("%s %7d caracteres\n", palabras[i], palabras[i].length());
+        }
+    }
 }
