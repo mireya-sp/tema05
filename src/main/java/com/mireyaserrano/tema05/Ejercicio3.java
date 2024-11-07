bc52e50d2a4ed6aa48429547a449cec7a6fa1209
= <=>
2024-11-07 19:13:34 +0100

    ej 3 estructuras de datos

--- src/main/java/com/mireyaserrano/tema05/Ejercicio3.java
+++ src/main/java/com/mireyaserrano/tema05/Ejercicio3.java
@@ -3,20 +3,20 @@ package com.mireyaserrano.tema05;
 import java.util.Scanner;
 
 public class Ejercicio3 {
+    /**
+     * divide una frase en palabras
+     * @param frase la frase que hay que dividir
+     * @return el numero de palabras que hay
+     */
+    public static int contarPalabras(String frase){
+        String[] palabras = frase.split("\\s+");
+        return palabras.length;
+    }
     public static void main(String args[]){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Itroduce una frase y te digo el número de palabras que contiene:");
         String frase = scanner.nextLine();
         scanner.close();
-        int espacios = 1;
-        int charsFrase = frase.length();
-        for (int i = 0; i < charsFrase; i++){
-            char letra = frase.charAt(i);
-            String letraStr = String.valueOf(letra);
-            if (letraStr.equals(" ")) {
-                espacios += 1;
-            }
-        }
-        System.out.printf("La frase tiene %d palabras", espacios);
+        System.out.printf("Hay %d palabras", contarPalabras(frase));
     }
 }
