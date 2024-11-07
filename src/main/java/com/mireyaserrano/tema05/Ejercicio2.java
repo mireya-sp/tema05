1a8fea079046a7043d53406e3e0b2c7f91af9cd0
= <=>
2024-11-07 19:08:04 +0100

    ej 2 estructuaras de datos

--- src/main/java/com/mireyaserrano/tema05/Ejercicio2.java
+++ src/main/java/com/mireyaserrano/tema05/Ejercicio2.java
@@ -2,27 +2,43 @@ package com.mireyaserrano.tema05;
 
 import java.util.Scanner;
 
-public class Ejercicio2 {//Escribe un programa que muestre el número de vocales y de consonantes de una frase
-    public static void main(String args[]){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Itroduce una frase y te digo el número de consonantes y de vocales:");//recorrer toda la frase??
-        String frase = scanner.nextLine();
-        scanner.close();
-        int mayusculas = 0;
-        int minusculas = 0;
-        int espacios = 0;
-        int charsFrase = frase.length();
-        for (int i = 0; i < charsFrase; i++){
-            char letra = frase.charAt(i);
-            String letraStr = String.valueOf(letra);
-            if (letraStr.equals(" ")) {
-                espacios += 1;
-            }else if (letraStr.equals(letraStr.toUpperCase())){
-                mayusculas += 1;
-            }else {
-                minusculas += 1;
+public class Ejercicio2 {
+
+    public static boolean esVocales(char c){
+        c = Character.toLowerCase(c);
+        String vocales = "aáàeéèiíoóòuú";
+        return vocales.indexOf(c) >= 0;
+    }
+
+    public static boolean esConsonantes(char c){
+        return Character.isLetter(c) && !esVocales(c);
+    }
+
+    public static int contarVocal(String texto){
+        int vocales = 0;
+        for (int i = 0; i < texto.length(); i++){
+            if (esVocales(texto.charAt(i))){
+                vocales++;
             }
         }
-        System.out.printf("En la frase hay %d mayusculas y %d minusculas", mayusculas, minusculas);
+        return vocales;
+    }
+
+    public static int contarConsonantes(String texto){
+        int consonantes = 0;
+        for (int i = 0; i < texto.length(); i++){
+            if (esConsonantes(texto.charAt(i))){
+                consonantes++;
+            }
+        }
+        return consonantes;
+    }
+
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce una frase");
+        String frase = scanner.nextLine();
+        scanner.close();
+        System.out.printf("Hay %d vocales y %d consonantes en la frase introducida", contarVocal(frase), contarConsonantes(frase));
     }
 }
