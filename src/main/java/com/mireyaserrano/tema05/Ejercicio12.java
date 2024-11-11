d03a1d16670d36737176b4aa6b9bfa4b7e24989d
= <=>
2024-11-11 20:09:57 +0100

    ej 12 estructuras de datos

--- src/main/java/com/mireyaserrano/tema05/Ejercicio12.java
+++ src/main/java/com/mireyaserrano/tema05/Ejercicio12.java
@@ -1,4 +1,16 @@
 package com.mireyaserrano.tema05;
 
+import java.util.Scanner;
+
 public class Ejercicio12 {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce una frase");
+        String frase = scanner.nextLine();
+        scanner.close();
+
+        frase = frase.replace("es", "no por");
+        frase = frase.replaceAll("\\d", "*");
+        System.out.println(frase);
+    }
 }
