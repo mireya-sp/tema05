4464d9d042f14c2655a497945a7d75a38e4739a4
Mireya Serrano <mireyaserrano181105@gmail.com>
2024-11-11 21:22:32 +0100

    ej 5 estructuras de datos

--- src/main/java/com/mireyaserrano/tema05/Ejercicio5.java
+++ src/main/java/com/mireyaserrano/tema05/Ejercicio5.java
@@ -3,18 +3,23 @@ package com.mireyaserrano.tema05;
 import java.util.Scanner;
 
 public class Ejercicio5 {
-    public static void main(String args[]){
+
+    public static String palabraLarga(String frase){
         String palabraLarga = "";
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Itroduce una frase y te digo cual es la palabra con más letras");
-        String frase = scanner.nextLine();
-        scanner.close();
         String [] palabras = frase.split("\\s+");
         for (int i = 0; i < palabras.length; i++){
             if (palabras[i].length() > palabraLarga.length()){
                 palabraLarga = palabras[i];
             }
         }
-        System.out.printf("La palabra más larga es [%s].", palabraLarga);
+        return palabraLarga;
+    }
+
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Itroduce una frase y te digo cual es la palabra con más letras");
+        String frase = scanner.nextLine();
+        scanner.close();
+        System.out.printf("La palabra más larga es [%s] y contiene %d caracteres.", palabraLarga(frase), palabraLarga(frase).length() );
     }
 }
