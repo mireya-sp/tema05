04bc4ff9d01db950fea2b4fd0b20bbe562209850
Mireya Serrano <mireyaserrano181105@gmail.com>
2024-11-12 13:12:51 +0100

    ej 18 estructuras de datos

--- src/main/java/com/mireyaserrano/tema05/Ejercicio18.java
+++ src/main/java/com/mireyaserrano/tema05/Ejercicio18.java
@@ -15,14 +15,29 @@ public class Ejercicio18 {
         return fecha;
     }
 
-    public static int primeraSuma(){
-
+    /**
+     * hace la suma de los números que se encuentren en el string fecha
+     * @param fecha la fecha a partir de la cual calculamos el luky number del usuario
+     * @return la suma de los números que contenga el string fecha introducido
+     */
+    public static int primeraSuma(String fecha){
+        String numeros = "";
+        int suma = 0;
+        for (int i = 0; i < fecha.length(); i++){
+            if (Character.isDigit(fecha.charAt(i))){
+                numeros += "" + fecha.charAt(i);
+            }
+        }
+        for (int j = 0; j < numeros.length(); j++){
+            char k = numeros.charAt(j);
+            suma += Character.getNumericValue(k);
+        }
+        return suma;
     }
 
     public static void main(String args[]){
         String fecha = solicitarFecha("Introduzca su fecha de nacimiento en formato dd-mm-aaaa:");
-
-
-
+        String primerResultado = "" + primeraSuma(fecha);
+        System.out.println("Tu 'luky number' es: " + primeraSuma(primerResultado));
     }
 }
