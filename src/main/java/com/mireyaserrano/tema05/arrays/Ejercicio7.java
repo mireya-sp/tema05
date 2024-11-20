7c6939fccf86e62c3e2bd9ba04734f0e017e306d
Mireya Serrano <mireyaserrano181105@gmail.com>
2024-11-20 13:51:23 +0100

    ej 7 arrays

---
+++ src/main/java/com/mireyaserrano/tema05/arrays/Ejercicio7.java
@@ -0,0 +1,41 @@
+package com.mireyaserrano.tema05.arrays;
+
+import com.mireyaserrano.tema05.lib.IO;
+
+public class Ejercicio7 {
+    /**
+     * imprime los multiplos del número que quieras
+     * @param array el array donde están los números que quieres saber si son múltiplos de el número
+     * @param multiplo el número del que tienen que ser múltiplo los elementos del array
+     */
+    public static void multiplos(int[] array, int multiplo){
+        System.out.println("\nMúltiplos de " + multiplo + ": ");
+        for (int i = 0; i < array.length; i++){
+            if (array[i] % multiplo == 0 && array[i] != 0){
+                System.out.print(array[i] + " ");
+            }
+        }
+    }
+
+    /**
+     * imprime los ceros del array
+     * @param array el array que va a recorrer buscando ceros
+     */
+    public static void ceros(int[] array){
+        System.out.println("\nCero: ");
+        for (int i = 0; i < array.length; i++){
+            if (array[i] == 0){
+                System.out.print(array[i] + " ");
+            }
+        }
+    }
+
+    public static void main(String args[]){
+        final int TAMANYO_ARRAY = 100;
+        int[] array = new int[TAMANYO_ARRAY];
+        IO.rellenarArray(array, 0, 100);
+        multiplos(array, 5);
+        ceros(array);
+        multiplos(array, 10);
+    }
+}
