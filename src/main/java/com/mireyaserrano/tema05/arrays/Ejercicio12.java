4e59554939c39ceade56947192e1caaedc0e5fe9
= <=>
2024-11-21 19:36:37 +0100

    ej 12 arrays

--- src/main/java/com/mireyaserrano/tema05/arrays/Ejercicio12.java
+++ src/main/java/com/mireyaserrano/tema05/arrays/Ejercicio12.java
@@ -1,26 +1,20 @@
 package com.mireyaserrano.tema05.arrays;
 
-import com.mireyaserrano.tema05.lib.IO;
 import com.mireyaserrano.tema05.lib.MetodosArrays;
 
 public class Ejercicio12 {
-
-    public static int llenarArrayP(int[] arrayP, int[] arrayV){
-        for (int i = 0; i < arrayP.length; i++){
-            if (i == 0){
-                arrayP[i] = arrayV[i];
-            }else{
-                arrayP[i] = arrayV[i] + (llenarArrayP(arrayP, arrayV));
-            }
-        }
-
-    }
-
     public static void main(String args[]){
         final int TAMANYO_ARRAY = 50;
         int[] arrayV = new int[TAMANYO_ARRAY];
         int[] arrayP = new int[TAMANYO_ARRAY];
         MetodosArrays.rellenarArray(arrayV, 0, 100);
-
+        for (int i = 0; i < arrayV.length;i++){
+            arrayP[i] = arrayV[i];
+            for (int j = 0; j < i;j++){
+                arrayP[i] = arrayP[i] + arrayV[j];
+            }
+        }
+        System.out.printf("Array V: %s\n", MetodosArrays.arrayToString(arrayV));
+        System.out.printf("Array P: %s\n", MetodosArrays.arrayToString(arrayP));
     }
 }
