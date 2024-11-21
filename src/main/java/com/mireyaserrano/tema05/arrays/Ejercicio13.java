bf840cbeb791a0f836ea3ebb9428f81f20d58f20
= <=>
2024-11-21 20:02:15 +0100

    ej 13 coments

--- src/main/java/com/mireyaserrano/tema05/arrays/Ejercicio13.java
+++ src/main/java/com/mireyaserrano/tema05/arrays/Ejercicio13.java
@@ -3,7 +3,11 @@ package com.mireyaserrano.tema05.arrays;
 import com.mireyaserrano.tema05.lib.MetodosArrays;
 
 public class Ejercicio13 {
-
+    /**
+     * devualve el número de pares de un array
+     * @param arrayV el array donde contamos los pares
+     * @return la cantidad de pares en el array
+     */
     public static int cuantosPares(int[] arrayV){
         int contador = 0;
         for (int i = 0; i < arrayV.length; i++){
@@ -14,7 +18,12 @@ public class Ejercicio13 {
         return contador;
     }
 
-    public static int[] llernarP(int[] arrayV, int contador){
+    /**
+     * llenar una array a partir de otro sus números pares
+     * @param arrayV array donde buscamos los pares y los metemos en otro array
+     * @return el array donde se guardan los pares
+     */
+    public static int[] llernarP(int[] arrayV){
         int j = 0;
         int[] arrayP = new int[cuantosPares(arrayV)];
         for (int i = 0; i < arrayV.length; i++){
@@ -34,7 +43,7 @@ public class Ejercicio13 {
             System.err.println("NO HAY PARES EN EL ARRAY ALEATORIO");
         }else {
             int[] arrayP = new int[cuantosPares(arrayV)];
-            arrayP = llernarP(arrayV, cuantosPares(arrayV));
+            arrayP = llernarP(arrayV);
             System.out.printf("Array V: %s\n", MetodosArrays.arrayToString(arrayV));
             System.out.printf("Array P: %s", MetodosArrays.arrayToString(arrayP));
         }
