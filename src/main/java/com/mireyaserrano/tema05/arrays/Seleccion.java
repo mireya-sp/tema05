0b8ffc5706f8fc7a724a227d837f0f942a29bab4
= <=>
2024-11-27 17:17:30 +0100

    .

--- src/main/java/com/mireyaserrano/tema05/arrays/Seleccion.java
+++ src/main/java/com/mireyaserrano/tema05/arrays/Seleccion.java
@@ -5,10 +5,16 @@ public class Seleccion {
 
     public static void ordenacionSeleccion(int[] array){
         for (int i = 0; i < array.length; i++){
+            int min = array[i];
+            int posicion = i;
             for (int j = i + 1; j < array.length; j++) {
-                if (array[i] > array[j]) {
-                    ordenacion.intercambio(array, j, i);
+                if (array[i] < array[j]) {
+                    min = array[j];
+                    posicion = j;
                 }
+            }
+            if (min < array[i]){
+                ordenacion.intercambio(array, i, posicion);
             }
         }
     }
