447d9cd57c16db4aaedbe08cda030d8e10f18485
Mireya Serrano <mireyaserrano181105@gmail.com>
2024-11-27 12:28:10 +0100

    método de ordenación por selección

---
+++ src/main/java/com/mireyaserrano/tema05/arrays/Seleccion.java
@@ -0,0 +1,19 @@
+package com.mireyaserrano.tema05.arrays;
+
+import java.util.Arrays;
+
+public class Seleccion {
+
+
+    public static void ordenacionSeleccion(int[] array){
+        for (int i = 0; i < array.length; i++){
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    ordenacion.intercambio(array, j, i);
+                }
+            }
+        }
+    }
+
+
+}
